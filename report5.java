7bfa11d682f8719d87a416518d95dbf81c5a79d0
Maeshiro Takao <e225757@ie.u-ryukyu.ac.jp>
2022-12-12 22:50:31 +0900

    2nd test

--- report5.java
+++ report5.java
@@ -1,6 +1,5 @@
 public class report5 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        System.out.println("test");
     }
 }

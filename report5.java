4b4c369724e25458cffab938df383ac09273d810
Maeshiro Takao <e225757@ie.u-ryukyu.ac.jp>
2022-12-20 09:04:54 +0900

    3rd commit

--- report5.java
+++ report5.java
@@ -1,12 +1,20 @@
 public class report5 {
-    public static void main(String[] args) {
+
+    /*public static void main(String[] args){
         try{
             String str = null;
             System.out.println(str.length());
         }catch(NullPointerException e){
             System.out.println("NullPointerExceptionが発生しました。"+ e.getMessage());
         }
+    }*/
+
+    public static void main(String[] args) {
+        String str = "壱百満";
+        System.out.println(str);
+        int value = Integer.parseInt(str);
     }
+}
 
     
-}
+
